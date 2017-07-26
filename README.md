9f73fa931955af682819902628a3c2dc62c1823b
Jonathan Whitaker <jonathan.whitaker@localhost.localdomain>
2017-07-26 13:06:50 -0400

    Add initial Hello, World application sources and build files.

---
+++ README.md
@@ -0,0 +1,5 @@
+hellapp
+=======
+
+Example Yocto/OpenEmbedded autotooled recipe, building an executable Hello, World example.
+
---
+++ helloapp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "helloapp.h"
+
+int main(int argc, char *argv[])
+{
+  printf("Hello Yocto World...\n");
+
+  return 0;
+}
+
+
+
+
+
